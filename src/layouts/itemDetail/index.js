4d9d79b494b314f0bc774d821dd30ac14f03ff40
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-03 17:19:31 +0900

    feat: enhance item detail layout with improved image display

--- src/layouts/itemDetail/index.js
+++ src/layouts/itemDetail/index.js
@@ -63,25 +63,8 @@ function ItemDetail() {
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox mt={2} mb={3}>
-        <MDBox mb={1}>
-          <Grid container spacing={2}>
-            {item &&
-              item.images.map((image, index) => (
-                <Grid item key={index} xs={12} sm={9} lg={4}>
-                  <img
-                    src={image}
-                    alt="image"
-                    loading="lazy"
-                    width="100%"
-                    height="300px"
-                    style={{ borderRadius: "8px" }}
-                  />
-                </Grid>
-              ))}
-          </Grid>
-        </MDBox>
         <Grid container spacing={3} mb={2} justifyContent="center">
-          <Grid item xs={12} sm={12}>
+          <Grid item xs={12} sm={8}>
             <Card>
               <MDBox p={2}>
                 <MDBox p={3}>
@@ -174,6 +157,23 @@ function ItemDetail() {
                 </MDBox>
               </MDBox>
             </Card>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Grid container spacing={2}>
+              {item &&
+                item.images.map((image, index) => (
+                  <Grid item key={index}>
+                    <img
+                      src={image}
+                      alt="image"
+                      loading="lazy"
+                      width="100%"
+                      height="300px"
+                      style={{ borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)" }}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
           </Grid>
         </Grid>
       </MDBox>
