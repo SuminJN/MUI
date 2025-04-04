d6c9f75f37e505076e3ed6bdc317b930ad9498a7
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-04 13:58:11 +0900

    refactor: comment out unused components in index.js and routes.js

--- src/layouts/profile/index.js
+++ src/layouts/profile/index.js
@@ -113,84 +113,84 @@ function Overview() {
           </MDBox>
         </MDBox>
 
-        <MDBox p={2}>
-          <Grid container spacing={6}>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultItemCard
-                image={homeDecor1}
-                label="label"
-                title="title"
-                description="As Uber works through a huge amount of internal management turmoil."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor2}
-                label="project #1"
-                title="scandinavian"
-                description="Music is something that everyone has their own specific opinion about."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor3}
-                label="project #3"
-                title="minimalist"
-                description="Different people have different taste, and various types of music."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor4}
-                label="project #4"
-                title="gothic"
-                description="Why would anyone pick blue over pink? Pink is obviously a better color."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
-              />
-            </Grid>
-          </Grid>
-        </MDBox>
+        {/*<MDBox p={2}>*/}
+        {/*  <Grid container spacing={6}>*/}
+        {/*    <Grid item xs={12} md={6} xl={3}>*/}
+        {/*      <DefaultItemCard*/}
+        {/*        image={homeDecor1}*/}
+        {/*        label="label"*/}
+        {/*        title="title"*/}
+        {/*        description="As Uber works through a huge amount of internal management turmoil."*/}
+        {/*        action={{*/}
+        {/*          type: "internal",*/}
+        {/*          route: "/pages/profile/profile-overview",*/}
+        {/*          color: "info",*/}
+        {/*          label: "view project",*/}
+        {/*        }}*/}
+        {/*      />*/}
+        {/*    </Grid>*/}
+        {/*    <Grid item xs={12} md={6} xl={3}>*/}
+        {/*      <DefaultProjectCard*/}
+        {/*        image={homeDecor2}*/}
+        {/*        label="project #1"*/}
+        {/*        title="scandinavian"*/}
+        {/*        description="Music is something that everyone has their own specific opinion about."*/}
+        {/*        action={{*/}
+        {/*          type: "internal",*/}
+        {/*          route: "/pages/profile/profile-overview",*/}
+        {/*          color: "info",*/}
+        {/*          label: "view project",*/}
+        {/*        }}*/}
+        {/*        authors={[*/}
+        {/*          { image: team3, name: "Nick Daniel" },*/}
+        {/*          { image: team4, name: "Peterson" },*/}
+        {/*          { image: team1, name: "Elena Morison" },*/}
+        {/*          { image: team2, name: "Ryan Milly" },*/}
+        {/*        ]}*/}
+        {/*      />*/}
+        {/*    </Grid>*/}
+        {/*    <Grid item xs={12} md={6} xl={3}>*/}
+        {/*      <DefaultProjectCard*/}
+        {/*        image={homeDecor3}*/}
+        {/*        label="project #3"*/}
+        {/*        title="minimalist"*/}
+        {/*        description="Different people have different taste, and various types of music."*/}
+        {/*        action={{*/}
+        {/*          type: "internal",*/}
+        {/*          route: "/pages/profile/profile-overview",*/}
+        {/*          color: "info",*/}
+        {/*          label: "view project",*/}
+        {/*        }}*/}
+        {/*        authors={[*/}
+        {/*          { image: team4, name: "Peterson" },*/}
+        {/*          { image: team3, name: "Nick Daniel" },*/}
+        {/*          { image: team2, name: "Ryan Milly" },*/}
+        {/*          { image: team1, name: "Elena Morison" },*/}
+        {/*        ]}*/}
+        {/*      />*/}
+        {/*    </Grid>*/}
+        {/*    <Grid item xs={12} md={6} xl={3}>*/}
+        {/*      <DefaultProjectCard*/}
+        {/*        image={homeDecor4}*/}
+        {/*        label="project #4"*/}
+        {/*        title="gothic"*/}
+        {/*        description="Why would anyone pick blue over pink? Pink is obviously a better color."*/}
+        {/*        action={{*/}
+        {/*          type: "internal",*/}
+        {/*          route: "/pages/profile/profile-overview",*/}
+        {/*          color: "info",*/}
+        {/*          label: "view project",*/}
+        {/*        }}*/}
+        {/*        authors={[*/}
+        {/*          { image: team4, name: "Peterson" },*/}
+        {/*          { image: team3, name: "Nick Daniel" },*/}
+        {/*          { image: team2, name: "Ryan Milly" },*/}
+        {/*          { image: team1, name: "Elena Morison" },*/}
+        {/*        ]}*/}
+        {/*      />*/}
+        {/*    </Grid>*/}
+        {/*  </Grid>*/}
+        {/*</MDBox>*/}
       </Header>
       <Footer />
     </DashboardLayout>
